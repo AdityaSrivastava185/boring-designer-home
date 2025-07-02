f379fc2d616808b711034e1cebc8fd453c83dd52
Aditya-Srivastava <pks85715@gmail.com>
2025-07-02 17:12:37 +0530

    Initial Commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {
+    // This option allows you to ignore TypeScript errors during the build process.
+    ignoreBuildErrors: true,
+  }
 };
 
 export default nextConfig;
