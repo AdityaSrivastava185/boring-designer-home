47855144f7e0be4c7637a4434a61cbd07b71da1f
Aditya-Srivastava <pks85715@gmail.com>
2025-07-02 17:18:42 +0530

    Nextjs configuration added

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint:{
+    ignoreDuringBuilds: true,
+  },
   typescript: {
     // This option allows you to ignore TypeScript errors during the build process.
     ignoreBuildErrors: true,
