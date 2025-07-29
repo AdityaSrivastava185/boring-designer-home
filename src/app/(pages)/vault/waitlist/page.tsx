60c65695538f2254a91a7e823b691919058a4660
Aditya-Srivastava <pks85715@gmail.com>
2025-07-29 22:14:19 +0530

    waitlist design

--- src/app/(pages)/vault/waitlist/page.tsx
+++ src/app/(pages)/vault/waitlist/page.tsx
@@ -2,11 +2,25 @@
 
 import React from 'react'
 import { Waitlist } from '@clerk/nextjs'
+import { Card, CardContent } from '@/components/ui/card'
 
 const Page = () => {
   return (
-    <div className="flex items-center justify-center h-screen waitlist-container">
-      <Waitlist />
+   <div className=" flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+       <Card className="overflow-hidden p-0">
+        <CardContent className="grid p-0 md:grid-cols-2">
+        <Waitlist />
+          <div className="bg-muted relative hidden md:block">
+            <img
+              src="/waitlist-image.jpg"
+              alt="Image"
+              className="absolute inset-0 h-full w-full object-cover grayscale "
+            />
+          </div>
+        </CardContent>
+      </Card>
+      </div>
     </div>
   )
 }
